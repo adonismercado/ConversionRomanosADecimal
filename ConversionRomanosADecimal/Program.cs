f7a504bc5303e8d77220d71ec0e810970d0e7c6c
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-01 23:54:14 -0400

    Agregacion del metodo estatico ValorRomano

--- ConversionRomanosADecimal/Program.cs
+++ ConversionRomanosADecimal/Program.cs
@@ -6,5 +6,18 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static int ValorRomano(char c)
+        {
+            if (c == 'I') return 1;
+            if (c == 'V') return 5;
+            if (c == 'X') return 10;
+            if (c == 'L') return 50;
+            if (c == 'C') return 100;
+            if (c == 'D') return 500;
+            if (c == 'M') return 1000;
+
+            throw new ArgumentException("Carácter romano inválido");
+        }
     }
 }

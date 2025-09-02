f356881b0087402c74704c3dd307f4c14fbe657e
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-01 23:50:32 -0400

    Add project files.

---
+++ ConversionRomanosADecimal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConversionRomanosADecimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
